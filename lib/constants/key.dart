8e44589013446fb786d8e9b2b3c8732bcd34f368
mac <mac@macs-MacBook-Pro.local>
2025-07-15 06:51:24 -0700

    recen screen draweer ui fixes sripe  env file

--- lib/constants/key.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-final stripePublishKey = dotenv.env['STRIPE_PUBLISHABLE_KEY'] ?? '';
-final stripeSecretKey = dotenv.env['STRIPE_SECRET_KEY'] ?? '';
